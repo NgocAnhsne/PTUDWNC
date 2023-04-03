ba27d8d7c06e4cf8e0f4e430051cf6cc852e9dc7
NgocAnhsne <ngocanhne2402@gmail.com>
2023-04-03 15:05:38 +0700

    Lab05_B.4 & 5

--- src/tat-blog/src/Pages/Index.js
+++ src/tat-blog/src/Pages/Index.js
@@ -1,13 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import PostItem from '../Components/PostItem';
+import { getPosts } from '../Services/BlogRepository';
 
 const Index = () => {
+    const [postList, setPostList] = useState([]);
     useEffect(() => {
         document.title = 'Trang chủ';
+
+        getPosts().then(data => {
+            if (data)
+                setPostList(data.items);
+            else
+                setPostList([]);
+        })
     }, []);
-    return (
-        <h1>
-            Đây là trang chủ
-        </h1>
+
+    if (postList.length > 0) 
+        return ( 
+            <div className='p-4'> 
+                {postList.map((item, index) => { 
+                    return ( 
+                        <PostItem postItem={item} key={index}/>
+                    );
+                })}
+            </div>
+        );
+    else return (
+        <></>
     );
+    // return (
+    //     <h1>
+    //         Đây là trang chủ
+    //     </h1>
+    // );
+
 }
 export default Index;
