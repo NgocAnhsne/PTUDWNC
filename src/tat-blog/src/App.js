392e0412e1325744e641352638ecc186fdc4b832
NgocAnhsne <ngocanhne2402@gmail.com>
2023-04-03 13:02:54 +0700

    Lab05_B.2

--- src/tat-blog/src/App.js
+++ src/tat-blog/src/App.js
@@ -1,24 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+import Sidebar from './Components/Sidebar';
+import Footer from './Components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <div>
+      <Router>
+        <Navbar/>
+        <div className='container-fluid'>
+          <div className='row'>
+            <div className='col-9'>
+
+            </div>
+            <div className='col-3 border-start'>
+              <Sidebar />
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </Router>
     </div>
+    
   );
 }
 
