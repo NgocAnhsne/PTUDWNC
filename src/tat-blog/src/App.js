41b4771cede4eada42b24b8966462736a3596c73
NgocAnhsne <ngocanhne2402@gmail.com>
2023-04-03 13:41:34 +0700

    Lab05_B.3

--- src/tat-blog/src/App.js
+++ src/tat-blog/src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
+import Sidebar from './Components/Sidebar';
+import Footer from './Components/Footer';
+import Index from './Pages/Index';
+import Layout from './Pages/Layout';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import RSS from './Pages/RSS';
 import {
   BrowserRouter as Router,
   Routes,
   Route
 } from 'react-router-dom';
-import Sidebar from './Components/Sidebar';
-import Footer from './Components/Footer';
+
 
 function App() {
   return (
@@ -33,7 +39,15 @@ function App() {
         <div className='container-fluid'>
           <div className='row'>
             <div className='col-9'>
-
+            <Routes>
+              <Route path='/' element={<Layout />}>
+                <Route path='blog' element={<Index />} />
+                <Route path='blog/Contact' element={<Contact />} />
+                <Route path='blog/About' element={<About />} />
+                <Route path='/' element={<Index />} />
+                <Route path='blog/RSS' element={<RSS />} />
+              </Route>
+            </Routes>
             </div>
             <div className='col-3 border-start'>
               <Sidebar />
