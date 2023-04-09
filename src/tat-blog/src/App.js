3d14c244b3c9c250a27f7731fca2eae2db6a29e8
NgocAnhsne <ngocanhne2402@gmail.com>
2023-04-09 23:24:47 +0700

    Update Lab06_B.2

--- src/tat-blog/src/App.js
+++ src/tat-blog/src/App.js
@@ -8,6 +8,12 @@ import About from './Pages/About';
 import Contact from './Pages/Contact';
 import RSS from './Pages/RSS';
 import AdminLayout from './Pages/Admin/Layout';
+import * as AdminIndex from './Pages/Admin/Index';
+import Authors from './Pages/Admin/Authors' 
+import Categories from './Pages/Admin/Categories' 
+import Comments from './Pages/Admin/Comments' 
+import Posts from './Pages/Admin/Post/Posts' 
+import Tags from './Pages/Admin/Tags' 
 import {
   BrowserRouter as Router,
   Routes,
@@ -51,6 +57,12 @@ function App() {
         <Route path='blog/RSS' element={<RSS />} />
         </Route>
         <Route path='/admin' element={<AdminLayout />} >
+          <Route path='/admin' element={<AdminIndex.default />} />
+          <Route path='/admin/authors' element={<Authors />} />
+          <Route path='/admin/categories' element={<Categories />} />
+          <Route path='/admin/comments' element={<Comments />} />
+          <Route path='/admin/posts' element={<Posts />} />
+          <Route path='/admin/tags' element={<Tags />} />
         </Route>
       </Routes>
       <Footer />
